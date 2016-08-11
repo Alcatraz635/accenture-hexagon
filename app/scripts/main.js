8acb9e9054c3037111b412e2782ec8634a191bbf
Tyler Schumacher <tylerschumacher635@gmail.com>
2016-08-11 16:59:39 -0400

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+const addAnimation = (target, animation = "hexagon-animation", reverseAnimation = "reverse-hexagon-animation") => {
+
+  $(`${target}`).find('.hexagon').addClass(animation)
+  $(`${target}`).find('.reverse-hexagon').addClass(reverseAnimation)
+}
+
+setTimeout( () => {
+  setTimeout(addAnimation('.container-one'))
+}, 1000)
+setTimeout( () => {
+  setTimeout(addAnimation('.container-four'))
+}, 2000)
+setTimeout( () => {
+  setTimeout(addAnimation('.container-three'))
+}, 3000)
+setTimeout( () => {
+  setTimeout(addAnimation('.container-two'))
+}, 4000)
+setTimeout( () => {
+  setTimeout(addAnimation('.container-five'))
+}, 5000)
